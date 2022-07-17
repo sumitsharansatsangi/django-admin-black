13998a65ec1c3286ea082a58d80acf8bf165d72b
Sumit Kumar <sharansumitkumar@gmail.com>
2022-07-17 23:39:20 +0530

    changes for custom user model

--- admin_black/models.py
+++ admin_black/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.utils.translation import gettext as _
 
 
 class AdminBlackSetting(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='admin_black_setting')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='admin_black_setting')
     sidebar_background = models.CharField(_('sidebar background'), max_length=20, default='primary')
     dark_mode = models.BooleanField(_('dark mode'), default=True)
     created_at = models.DateTimeField(_("created at"), auto_now_add=True)
